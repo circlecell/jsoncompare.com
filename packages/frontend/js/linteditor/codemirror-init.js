825dabdec36f603bd1a26750b70e0c3b884dcea1
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2017-06-05 19:12:14 +0300

    feat: Search with ctrl + f

--- packages/frontend/js/linteditor/codemirror-init.js
+++ packages/frontend/js/linteditor/codemirror-init.js
@@ -2,10 +2,21 @@ import 'codemirror/mode/javascript/javascript';
 import 'codemirror/addon/merge/merge.css';
 import 'codemirror/lib/codemirror.css';
 
+// Fold
 import 'codemirror/addon/fold/foldgutter.css';
 import 'codemirror/addon/fold/foldcode.js';
 import 'codemirror/addon/fold/foldgutter.js';
 import 'codemirror/addon/fold/brace-fold.js';
+
+// Search
+import 'codemirror/addon/dialog/dialog.css';
+import 'codemirror/addon/search/matchesonscrollbar.css';
+import 'codemirror/addon/dialog/dialog.js';
+import 'codemirror/addon/search/searchcursor.js';
+import 'codemirror/addon/search/search.js';
+import 'codemirror/addon/scroll/annotatescrollbar.js';
+import 'codemirror/addon/search/matchesonscrollbar.js';
+import 'codemirror/addon/search/jump-to-line.js';
 
 import assign from 'object.assign';
 import CodeMirror from 'codemirror';
